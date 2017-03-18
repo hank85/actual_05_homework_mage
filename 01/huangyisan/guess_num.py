5302239364da260631c5b05a117d7285be3c8109
huangyisan <ahuangyisan@live.cn>
2017-03-18 23:25:13 +0800

    add color

--- 01/huangyisan/guess_num.py
+++ 01/huangyisan/guess_num.py
@@ -13,14 +13,14 @@ import random
 real_num=random.randint(0,100)
 count=5
 while count:    #当count自减到0，则跳出。
-    print('总共5次机会，当前还剩下%s次' %count)
+    print('总共5次机会，当前还剩下%s次~' %count)
     try:    #捕获非整数输入的错误。
         guess_num = int(input('enter a num: '))
     except ValueError as e:
-        print('你敢不敢输入一个整数，再捣乱抽你哦~')
+        print('\033[1;31;40m你敢不敢输入一个整数，再捣乱抽你哦~\033[0m')
     else:
         if guess_num == real_num:
-            print('猜到了')
+            print('\033[1;33;41m猜到了\033[0m')
             break
         elif guess_num < real_num:
             count -= 1
@@ -29,4 +29,4 @@ while count:    #当count自减到0，则跳出。
             count -= 1
             print('猜大了')
 else:
-    print('太笨了，下次再来')
+    print("\033[1;31;40m太笨了，下次再来 \033[0m")
