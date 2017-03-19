5919c2c9e30c0b69738940dd650cae485e3f13bd
root <root@localhost.localdomain>
2017-03-19 19:08:19 +0800

    修改格式的99乘法表

--- 01/yaoyongxin/99.py
+++ 01/yaoyongxin/99.py
@@ -3,6 +3,6 @@ a=[1,2,3,4,5,6,7,8,9]
 for i in a:
     b=1
     while b <= i:
-        print(b,'X',i,'=',b*i,' ',end='')
+        print('{}X{}{}{:<4}'.format(b,i,'=',b*i),end='')
         b+=1
     print()
