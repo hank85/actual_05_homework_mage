231df93d6bfb2dc40ce99963b20b34c0cb1fb14c
KeithTt <329640305@qq.com>
2017-03-20 12:39:00 +0800

    modify 99.py

--- 01/keithtt/99.py
+++ 01/keithtt/99.py
@@ -3,7 +3,7 @@
 
 for i in range(1,10): 
     for j in range(1,1+i):
-        print(str(j)+"*"+str(i)+"="+str(i*j),end="")
+        print(str(j) + "*" + str(i) + "=" + str(i*j), end="")
     print() 
  
 for i in range(1,10): 
@@ -11,4 +11,4 @@ for i in range(1,10):
        print("%d*%d=%2d" % (i,j,i*j), end ="")
    print()
      
-print ('\n'.join([' '.join(['%s*%s=%-2s' % (j,i,i*j) for j in range(1,i+1)]) for i in range(1,10)]))
+print ('\n'.join([''.join(['%s*%s=%-2s' % (j,i,i*j) for j in range(1,i+1)]) for i in range(1,10)]))
