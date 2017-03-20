678583d7b4c60274c190085156951ced7b659007
root <root@localhost.localdomain>
2017-03-20 05:41:42 -0400

    first commit

--- 01/wangfz/99chengfabiao.py
+++ 01/wangfz/99chengfabiao.py
@@ -1,11 +1,11 @@
 #encoding:utf-8
 #打印乘法口诀表
 i = 1
-while (i <=9):
+while (i <=9 ):
     j = 1
     while j <= i:
-        x = j * i
-        print(i,'*',j,'=',x,"\t",end=" ")
-        i += 1
+        x = i * j
+        print(j,'*',i,'=',x,"  ",end=" ")
+        j += 1
     print("\n")
-    j += 1
+    i += 1
