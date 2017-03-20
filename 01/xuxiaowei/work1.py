911826aee5a0f335717fef4c190b249d0325ddfe
xuxiaowei <719822701@qq.com>
2017-03-20 16:15:47 +0800

    第一课作业

---
+++ 01/xuxiaowei/work1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.5
+#encoding: utf-8
+
+#乘法口诀
+
+i = 1
+
+while i < 10:
+    j = 1
+    while j < 10:
+        print(i, '*', j ,'=', i * j,end=' | ')
+        j += 1
+    print('\n')
+    i += 1
