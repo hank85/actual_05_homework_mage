c54ca0fd0a607eed08dd973001ca5ec586b9a895
root <root@python.web.com>
2017-03-26 10:56:19 +0800

    first commit

--- 02/taokecheng/homework.py
+++ 02/taokecheng/homework.py
@@ -13,38 +13,35 @@ import random
 #     6.重复步骤2~5
 
 #生成一个随机数组
+
 n=0
 random_list = []
-while n <8:
+while n <7:
 	num  =  random.randint(0,100)
 	if num not in random_list:
 		random_list.append(num)
 		n += 1
 
 print(random_list)
-insert_num = int(input('以上是一个随机的列表，请输入要出入列表的数据进行排序：'))
+insert_num = int(input('以上是一个随机的列表,插入一个数字与其排序：'))
 input_list = [insert_num] + random_list
 
 for i in range(1,len(input_list)):
 	key = input_list[i]
+	index = i 
 	for j in range(i-1,-1,-1):
 		if input_list[j] > key:
 			input_list[j+1] = input_list[j]
+			index -= 1 
 		else:
 			break
-		input_list[j] = key
+	input_list[index] = key
 
 
 print('插入排序结果：')
 print(input_list)
 
 #二分查找法
-# [一维数组，折半查找]
-# 假如有一组数为3，12，24，36，55，68，75，88要查给定的值24.可设三个变量front，mid，end分别指向数据的上界，中间和下界，mid=（front+end）/2.
-# 1.开始令front=0（指向3），end=7（指向88），则mid=3（指向36）。因为mid>x，故应在前半段中查找。
-# 2.令新的end=mid-1=2，而front=0不变，则新的mid=1。此时x>mid，故确定应在后半段中查找。
-# 3.令新的front=mid+1=2，而end=2不变，则新的mid=2，此时a[mid]=x，查找成功。
-# 如果要查找的数不是数列中的数，例如x=25，当第三次判断时，x>a[mid]，按以上规律，令front=mid+1，即front=3，出现front>end的情况，表示查找不成功。
 
 print('二分法从以上列表查找数据。')
 search_num = int(input('请输入要查找的数字：'))
